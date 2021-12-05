0701916d38c11388161da4db06f2d0fef1802ead
Adarsh S Kumar <adarsh@urbanrider.in>
2021-12-05 21:58:24 +0530

    hack 4tkm by team beta tech

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/adarshskumar/FlutterDev/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/adarshskumar/Desktop/Beta Tech/phone-auth"
+export "FLUTTER_APPLICATION_PATH=/Users/adarshskumar/Hack4Tkm-Beta-Tech"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=/Users/adarshskumar/Desktop/Beta Tech/phone-auth/lib/main.dart"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
-export "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
 export "DART_OBFUSCATION=false"
-export "TRACK_WIDGET_CREATION=true"
+export "TRACK_WIDGET_CREATION=false"
 export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=/Users/adarshskumar/Desktop/Beta Tech/phone-auth/.dart_tool/package_config.json"
+export "PACKAGE_CONFIG=.packages"
